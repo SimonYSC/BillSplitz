eac0fae9a96f552741dfe878ce2de5d742a33f8b
Simon Chao <simon.y.chao@gmail.com>
2025-11-17 22:26:01 -0500

    Initial Commit

---
+++ BillSplitz/BillSplitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitzApp.swift
+//  BillSplitz
+//
+//  Created by Simon Chao on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillSplitzTests/BillSplitzTests.swift
@@ -0,0 +1,16 @@
+//
+//  BillSplitzTests.swift
+//  BillSplitzTests
+//
+//  Created by Simon Chao on 11/17/25.
+//
+
+import Testing
+
+struct BillSplitzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
